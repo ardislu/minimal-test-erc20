19c9f14c34a230561864f3f4f7da6c2427f411bd
Ardis Lu <ardisglu@gmail.com>
2024-05-10 22:50:53 -0700

    Add transferFrom() test

--- test/SMTChecker.sol
+++ test/SMTChecker.sol
@@ -30,4 +30,25 @@ contract SMTChecker is ERC20 {
     assert(this.balanceOf(address(this)) == 0);
     assert(this.balanceOf(rand) == amount);
   }
+
+  // transferFrom() updates balances and allowance as expected and does not modify totalSupply
+  function transferFromUpdatesBalances(address randFrom, address randTo, uint256 amount) public {
+    // Preconditions:
+    require(address(this) != randFrom);
+    require(address(this) != randTo);
+    require(randFrom != randTo);
+    require(this.totalSupply() == amount);
+    require(this.balanceOf(randFrom) == amount);
+    require(this.balanceOf(randTo) == 0);
+    require(this.allowance(randFrom, address(this)) == amount);
+
+    // Action:
+    this.transferFrom(randFrom, randTo, amount);
+
+    // Postconditions:
+    assert(this.totalSupply() == amount);
+    assert(this.balanceOf(randFrom) == 0);
+    assert(this.balanceOf(randTo) == amount);
+    assert(this.allowance(randFrom, address(this)) == 0);
+  }
 }
